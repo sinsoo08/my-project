4be0e26d4192e944c4fa6b65c77435dbf34716ab
Shin ji soo <white_kite08@naver.com>
2025-03-11 19:20:43 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int A, B, sum;
+	    A = sc.nextInt();
+	    B = sc.nextInt();
+	    sum = A - B;
+	    System.out.println(sum);
+		
+	}
+}
