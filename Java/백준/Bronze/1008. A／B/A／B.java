eb771107768df18e5fa62ac9669a82b6af15f1be
Shin ji soo <white_kite08@naver.com>
2025-03-12 20:09:20 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 18012 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    double A, B;
+	    Scanner sc = new Scanner(System.in);
+	    A = sc.nextDouble();
+	    B = sc.nextDouble();
+		System.out.println(A / B);
+	}
+}
