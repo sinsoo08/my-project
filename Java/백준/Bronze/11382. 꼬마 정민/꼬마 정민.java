858b74f4dd4b3acbed958952907c252a929e5768
Shin ji soo <white_kite08@naver.com>
2025-03-14 23:12:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17628 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		long A, B, C;
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextLong();
+		B = sc.nextLong();
+		C = sc.nextLong();
+		System.out.println(A + B + C);
+	}
+}
