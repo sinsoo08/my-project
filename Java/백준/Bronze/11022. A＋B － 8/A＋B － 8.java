1cd771c36494a5fd257f311eb7feb7f808480a34
Shin ji soo <white_kite08@naver.com>
2025-03-22 02:52:06 +0900

    [Bronze V] Title: A+B - 8, Time: 224 ms, Memory: 18948 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int T, A, B;
+	    int sum = 0;
+	    T = sc.nextInt();
+	    for(int i = 1; i <= T; i++){
+	        A = sc.nextInt();
+	        B = sc.nextInt();
+	        sum = A + B;
+	        System.out.println("Case #" + i + ": " + A + " + " + B + " = " + sum);
+	    }
+	}
+}
