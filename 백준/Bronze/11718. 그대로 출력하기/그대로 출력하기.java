491cd2adf252643e1ba58a4dd3dceaeaef287da6
Shin ji soo <white_kite08@naver.com>
2025-04-02 19:42:55 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    while(sc.hasNext()){
+	        String a = sc.nextLine();
+	        System.out.println(a);
+	    }
+	    sc.close();
+	}
+}
