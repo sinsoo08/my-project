1b1a76bd954705a012285b38360e0bee57b68ec4
Shin ji soo <white_kite08@naver.com>
2025-04-09 19:04:16 +0900

    [Bronze II] Title: 진법 변환, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String N = sc.next();
+	    int a = sc.nextInt();
+	    int b = Integer.parseInt(N, a);
+	    System.out.println(b);
+	}
+}
