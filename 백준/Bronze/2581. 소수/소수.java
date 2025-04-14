2c4a31750ec79570273ae7e363125d5f53b1df5d
Shin ji soo <white_kite08@naver.com>
2025-04-14 18:42:16 +0900

    [Bronze II] Title: 소수, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (i < min) {
+                    min = i;
+                }
+            }
+        }
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
