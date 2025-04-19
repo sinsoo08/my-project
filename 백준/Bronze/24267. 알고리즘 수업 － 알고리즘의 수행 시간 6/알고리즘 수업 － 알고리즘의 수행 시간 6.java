c8829827e257e9b608635df8a57fe2ff897da718
Shin ji soo <white_kite08@naver.com>
2025-04-20 03:01:08 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        System.out.println((a*(a-1)*(a-2))/6);
+        System.out.println(3);
+    }
+}
