fbf0d7823ae0bde3968cc8efe376e08a11ebe198
Shin ji soo <white_kite08@naver.com>
2025-05-08 10:31:55 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0;
+        while (n >= 5) {
+            c += n / 5;
+            n /= 5;
+        }
+        System.out.println(c);
+    }
+}
