a6116d74de77cd22af023c96e25d560eb59638ca
Shin ji soo <white_kite08@naver.com>
2025-05-13 19:46:13 +0900

    [Silver IV] Title: 큐, Time: 444 ms, Memory: 35824 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
+            switch (s) {
+                case "push":
+                    int x = sc.nextInt();
+                    queue.offer(x);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    System.out.println(queue.isEmpty() ? 1 : 0);
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.peek());
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(((LinkedList<Integer>)queue).getLast());
+                    }
+                    break;
+            }
+        }
+    }
+}
