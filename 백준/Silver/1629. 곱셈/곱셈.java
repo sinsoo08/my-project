3a369330dab5cddcffc11d7eff68a06e2c7894e5
Shin ji soo <white_kite08@naver.com>
2025-05-14 10:09:03 +0900

    [Silver I] Title: 곱셈, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static long pow(long a, long b, long c) {
+        if (b == 0) return 1;
+        long half = pow(a, b / 2, c);
+        long result = (half * half) % c;
+        if (b % 2 == 1) {
+            result = (result * a) % c;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(pow(A, B, C));
+    }
+}
