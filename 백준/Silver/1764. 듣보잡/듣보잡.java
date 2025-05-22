47b79fa6197ac2997a65f08209315d77d6220828
Shin ji soo <white_kite08@naver.com>
2025-05-23 02:11:52 +0900

    [Silver IV] Title: 듣보잡, Time: 272 ms, Memory: 25920 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Set<String> unheard = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            unheard.add(br.readLine());
+        }
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (unheard.contains(name)) {
+                result.add(name);
+            }
+        }
+        Collections.sort(result);
+        System.out.println(result.size());
+        for (String name : result) {
+            System.out.println(name);
+        }
+    }
+}
