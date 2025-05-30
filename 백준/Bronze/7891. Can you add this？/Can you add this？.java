c90e6051396af8f723f362458ffe9f5636438c00
Shin ji soo <white_kite08@naver.com>
2025-05-30 10:28:42 +0900

    [Bronze V] Title: Can you add this?, Time: 192 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    for(int i = 0; i < t; i++){
+	        int sione = sc.nextInt();
+	        int sitwo = sc.nextInt();
+	        System.out.println(sione + sitwo);
+	    }
+	}
+}
