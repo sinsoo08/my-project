44eb9960fd18a2ce875a29544103954377d4cf9c
Shin ji soo <white_kite08@naver.com>
2025-05-31 21:07:24 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 184 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,13 @@
+import java.math.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = in.nextBigInteger();
+        BigInteger m = in.nextBigInteger();
+        in.close();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+
+    }
+}
