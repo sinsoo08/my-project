031d1e8a333373fe4601e13dbb29c0e510a29ae0
Shin ji soo <white_kite08@naver.com>
2025-06-03 00:02:37 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,45 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		StringBuilder sb = new StringBuilder();
-		
-		StringTokenizer st;
-		
-		for(int i = 0; i < N; i++) {
-			
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			int d = gcd(a, b);
-			
-			sb.append(a * b / d).append('\n');
-		}
-		System.out.println(sb);
- 
-	}
-    
-    
-	// 최대공약수 반복문 방식
-	public static int gcd(int a, int b) {
- 
+    public static int gcd(int a, int b) {
 		while (b != 0) {
-			int r = a % b; // 나머지를 구해준다.
- 
-			// GCD(a, b) = GCD(b, r)이므로 변환한다.
+			int r = a % b;
 			a = b;
 			b = r;
 		}
 		return a;
 	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int d = gcd(a, b);
+			sb.append(a * b / d).append('\n');
+		}
+		System.out.println(sb);
+    }
 }
