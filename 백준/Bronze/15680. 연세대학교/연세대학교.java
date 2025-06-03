86d6e84051fb94e4dfe50bbece5a8f024e209b63
Shin ji soo <white_kite08@naver.com>
2025-06-03 21:25:39 +0900

    [Bronze V] Title: 연세대학교, Time: 168 ms, Memory: 17512 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int i = sc.nextInt();
+      if(i == 0){
+         System.out.println("YONSEI");
+      }
+      else System.out.println("Leading the Way to the Future");
+    }
+}
