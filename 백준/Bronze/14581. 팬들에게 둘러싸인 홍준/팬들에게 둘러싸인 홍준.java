e22acc710f4d2cb9b9d3f5f09fb521fe24bfdfbb
Shin ji soo <white_kite08@naver.com>
2025-06-23 18:28:11 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 196 ms, Memory: 18204 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + a + "::fan:");
+		System.out.println(":fan::fan::fan:");
+		sc.close();
+	}
+}
