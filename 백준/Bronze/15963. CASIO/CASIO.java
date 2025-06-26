da35460c6ab12150a004fe5514acb70b7e3d1159
Shin ji soo <white_kite08@naver.com>
2025-06-26 22:59:30 +0900

    [Bronze V] Title: CASIO, Time: 180 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		if(n == m){
+			System.out.println(1);
+		}
+		else System.out.println(0);
+    }
+}
