f9e7db3ff786848c4379c45e4d53eaceb0bdc0ad
Shin ji soo <white_kite08@naver.com>
2025-06-28 23:03:21 +0900

    [Bronze V] Title: 삼각형, Time: 184 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double w = sc.nextDouble();
+		double h = sc.nextDouble();
+		double area = w * h / 2;
+		System.out.println(String.format("%.1f", area));
+		sc.close();
+	}
+}
