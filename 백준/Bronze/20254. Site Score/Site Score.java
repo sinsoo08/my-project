ccdf0ebbceb2611c84701ab9e31ad2520a57a746
Shin ji soo <white_kite08@naver.com>
2025-07-01 22:01:49 +0900

    [Bronze V] Title: Site Score, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int u = sc.nextInt();
+        int t = sc.nextInt();
+        int u1 = sc.nextInt();
+        int t1 = sc.nextInt();
+        System.out.print(56*u + 24*t + 14*u1 + 6 *t1);
+    }
+}
