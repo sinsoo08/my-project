56a3096f8c3a84ea2afa25cf0a9fb5852626104f
Shin ji soo <white_kite08@naver.com>
2025-07-12 23:29:52 +0900

    [Bronze V] Title: 팩토리얼 3, Time: 2996 ms, Memory: 387976 KB -BaekjoonHub

---
+++ 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.valueOf(br.readLine());
+        System.out.println(factorial(1, N));
+    }
+    public static BigInteger factorial(int a, int n){
+        BigInteger num = BigInteger.valueOf(a);
+        if (a < n){
+            int b = (a + n) / 2;
+            num = factorial(a,b).multiply(factorial(b+1, n));
+        }
+        return num;
+    }
+}
