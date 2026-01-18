b507ea5697b8e0dac806af817c04bd6d3fdd2bd1
Shin ji soo <white_kite08@naver.com>
2026-01-19 00:51:12 +0900

    [Bronze IV] Title: 시험 점수, Time: 164 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0, b = 0, t;
+        for (int i = 0; i < 4; i++){
+            t = sc.nextInt();
+            a += t;
+        }
+        for (int i = 0; i < 4; i++){
+            t = sc.nextInt();
+            b += t;
+        }
+        System.out.println(Math.max(a, b));
+    }
+}
