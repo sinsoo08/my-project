16cf069c13f45832df26b4ee78079de327cbc5dd
Shin ji soo <white_kite08@naver.com>
2026-03-26 20:13:38 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

---
+++ Java/백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    
+	    if(a > b){
+	        System.out.println(b);
+	    }
+	    else if(b > a){
+	        System.out.println(a);
+	    }
+	    
+	    else{
+	        System.out.println(a);
+	    }
+	}
+}
