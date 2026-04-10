9121748f3d8f3b6834f535c30345c3ca21cb2293
Shin ji soo <white_kite08@naver.com>
2026-04-10 17:54:02 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ Java/백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 이 줄 추가
+        
+        int sum = 0;
+        
+        for(int i = 0; i < 4; i++){
+            sum += Integer.parseInt(br.readLine());
+        }
+        
+        if(sum <= 1500){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
